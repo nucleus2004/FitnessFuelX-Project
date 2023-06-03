ce43e6d8de0372f356036bf053aacc575082d7d8
abdelrahmmann <abdelrahmanebid.12346@gmail.com>
2023-06-04 02:01:20 +0300

    new changes and profile, userProfile

--- assets/js/signs.js
+++ assets/js/signs.js
@@ -88,3 +88,15 @@ function validateForm() {
               });
 
 ////////////////////////////////////////////////////////////////////////////
+
+document.getElementById('fileButton').addEventListener('click', function() {
+  document.getElementById('fileInput').click();
+});
+
+document.getElementById('fileInput').addEventListener('change', function() {
+  var fileName = this.value.split('\\').pop();
+  if (fileName) {
+    // Display the selected file name or perform any other desired action
+    console.log('Selected file:', fileName);
+  }
+});
--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -165,4 +165,14 @@ comments.forEach(comment => {
 // });
 
 
+document.getElementById('fileButton').addEventListener('click', function() {
+  document.getElementById('fileInput').click();
+});
 
+document.getElementById('fileInput').addEventListener('change', function() {
+  var fileName = this.value.split('\\').pop();
+  if (fileName) {
+    // Display the selected file name or perform any other desired action
+    console.log('Selected file:', fileName);
+  }
+});
